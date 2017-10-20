84411c79abd299e3da36829b567aa0307d101eb4
twojcicki <twojcicki@sygnity.pl>
2017-10-20 13:06:04 +0200

    Breadcrumbs

---
+++ src/main/java/com/jfeesoft/office/view/BreadCrumbBean.java
@@ -0,0 +1,42 @@
+package com.jfeesoft.office.view;
+
+import javax.annotation.PostConstruct;
+
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.MenuModel;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("breadCrumbBean")
+@Scope("session")
+public class BreadCrumbBean {
+	private MenuModel model;
+	private DefaultMenuItem homeItem;
+
+	@PostConstruct
+	public void init() {
+		model = new DefaultMenuModel();
+		homeItem = new DefaultMenuItem();
+		homeItem.setHref("/");
+		homeItem.setIcon("ui-icon-home");
+		model.addElement(homeItem);
+	}
+
+	public void addElement(String name, String href) {
+		model.getElements().clear();
+		model.addElement(homeItem);
+		DefaultMenuItem item = new DefaultMenuItem();
+		item.setHref(href);
+		item.setValue(name);
+		model.addElement(item);
+	}
+
+	public MenuModel getModel() {
+		return model;
+	}
+
+	public void setModel(MenuModel model) {
+		this.model = model;
+	}
+}

1ac3a04426db0a21d09accb84ae4f7fe8a3bd315
rutko <rutko@DESKTOP-O87R0MJ>
2017-11-17 10:58:00 +0100

    JaRutkowski com v0.2
    Adding domain Task class.

---
+++ src/main/java/com/jfeesoft/office/model/Task.java
@@ -0,0 +1,57 @@
+package com.jfeesoft.office.model;
+
+import java.util.Date;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "task")
+@AttributeOverride(name = "id", column = @Column(name = "task_id", nullable = false))
+@SequenceGenerator(name = "default_gen", sequenceName = "task_seq", allocationSize = 1, initialValue = 100)
+public class Task extends GenericEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "name", length = 64)
+	private String name;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_from", length = 13)
+	private Date dateFrom;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_to", length = 13)
+	private Date dateTo;
+
+	//TODO: realization state
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public Date getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+}

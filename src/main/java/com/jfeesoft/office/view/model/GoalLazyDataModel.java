16e93eea2e3049ea838b2d78b7d25407e10f294b
rutko <rutko@DESKTOP-O87R0MJ>
2017-11-17 11:26:49 +0100

    JaRutkowski com v0.2
    Changes in GoalLazyModel

--- src/main/java/com/jfeesoft/office/view/model/GoalLazyDataModel.java
+++ src/main/java/com/jfeesoft/office/view/model/GoalLazyDataModel.java
@@ -2,11 +2,11 @@ package com.jfeesoft.office.view.model;
 
 import org.springframework.stereotype.Component;
 
-import com.jfeesoft.office.model.Role;
+import com.jfeesoft.office.model.Goal;
 import com.jfeesoft.office.service.GoalService;
 
 @Component
-public class GoalLazyDataModel extends GenericLazyDataModel<Role> {
+public class GoalLazyDataModel extends GenericLazyDataModel<Goal> {
 
 	private static final long serialVersionUID = 1L;
 

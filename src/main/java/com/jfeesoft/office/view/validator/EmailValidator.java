5ff9697ff52d11913852b0dbb9b1c0e3993cbd8b
rutko <rutko@DESKTOP-O87R0MJ>
2017-11-27 08:17:11 +0100

    JaRutkowski Refactoring EmailValidator
    Adding Constans class, changing resource bundle and organising imports.

--- src/main/java/com/jfeesoft/office/view/validator/EmailValidator.java
+++ src/main/java/com/jfeesoft/office/view/validator/EmailValidator.java
@@ -1,22 +1,21 @@
 package com.jfeesoft.office.view.validator;
 
-import com.jfeesoft.office.service.SystemUserService;
-import org.primefaces.validate.ClientValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 import javax.faces.application.Application;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import javax.inject.Named;
-import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.jfeesoft.office.service.SystemUserService;
+import com.jfeesoft.office.view.utils.Constans;
 
 @Component
 @Scope("request")
@@ -25,15 +24,13 @@ public class EmailValidator implements Validator {
     @Autowired
     private SystemUserService userService;
 
-    private ResourceBundle messagesBundle;
+    @SuppressWarnings("unused")
+	private ResourceBundle messagesBundle;
 
     private Pattern pattern;
 
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
     public EmailValidator() {
-        pattern = Pattern.compile(EMAIL_PATTERN);
+        pattern = Pattern.compile(Constans.EMAIL_PATTERN);
     }
 
     @Override
@@ -43,14 +40,14 @@ public class EmailValidator implements Validator {
 
         if(!pattern.matcher(value.toString()).matches()) {
             FacesMessage msg =
-                    new FacesMessage(null,messagesBundle.getString("error.email.bad"));
+                    new FacesMessage(null,messagesBundle.getString("message.error.incorrectEmail"));
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(msg);
         }
 
         if(userService.existEmail((String)value)){
             FacesMessage msg =
-                    new FacesMessage(null,messagesBundle.getString("error.email.exist"));
+                    new FacesMessage(null,messagesBundle.getString("message.error.emailConstraintViolation"));
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(msg);
         }
